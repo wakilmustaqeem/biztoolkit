6820bcde5e50d153e777e6ecc88bb949e9111152
Muhammad Mustaqeem <mustaqeemfm@gmail.com>
2026-08-27 02:00:21 +0500

    Fix AdSense verification: use beforeInteractive script strategy

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5184606115658751"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </body>
     </html>
