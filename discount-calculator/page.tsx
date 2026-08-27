efdf3a6bd9bf2ce993b177340fd010df51d1103e
Muhammad Mustaqeem <mustaqeemfm@gmail.com>
2026-08-27 21:57:56 +0500

    apply rose theme to discount calculator

--- discount-calculator/page.tsx
+++ discount-calculator/page.tsx
@@ -74,7 +74,7 @@ const faqJsonLd = {
 
 export default function DiscountCalculatorPage() {
   return (
-    <main className="min-h-screen bg-gray-50 py-8">
+    <main className="min-h-screen bg-gradient-to-br from-rose-50 via-white to-red-100 py-8">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(webAppJsonLd) }}
